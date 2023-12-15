3368ebc463b90312a9d9f216d06e85c11a2dbd1c
inkerton <janvichoudhary116@gmail.com>
2023-12-16 01:29:18 +0530

    blog details page changed

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -3,44 +3,44 @@ import { Blog } from "@/types/blog";
 const blogData: Blog[] = [
   {
     id: 1,
-    title: "Best UI components for modern websites",
+    title: "How SBOM Empowers Developers and Small Businesses",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "Discover how Secure-compose may help developers and small businesses ensure security, efficiency, and creativity in software development and business operations.",
     image: "/images/blog/blog-01.jpg",
     author: {
-      name: "Samuyl Joshi",
-      image: "/images/blog/author-01.png",
+      name: "Janvi Choudhary",
+      image: "/images/blog/author-01.jpg",
       designation: "Graphic Designer",
     },
-    tags: ["creative"],
+    tags: ["empowering"],
     publishDate: "2025",
   },
   {
     id: 2,
-    title: "9 simple ways to improve your design skills",
+    title: "Future-Proofing Your Projects: Embracing the SBOM Revolution",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "The pivotal role of Software Bill of Materials (SBOM) in controlling vulnerabilities, providing transparency, risk assessment, and proactive security measures in software ecosystems.",
     image: "/images/blog/blog-02.jpg",
     author: {
-      name: "Musharof Chy",
-      image: "/images/blog/author-02.png",
+      name: "Bhumika Sharma",
+      image: "/images/blog/author-02.jpg",
       designation: "Content Writer",
     },
-    tags: ["computer"],
+    tags: ["need"],
     publishDate: "2025",
   },
   {
     id: 3,
-    title: "Tips to quickly improve your coding speed.",
+    title: "All you need to know about Secure-compose",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "It reinforces Secure-compose&apros;s role in providing transparency, facilitating vulnerability management, and supporting informed decision-making across the software development and procurement lifecycle.",
     image: "/images/blog/blog-03.jpg",
     author: {
-      name: "Lethium Deo",
-      image: "/images/blog/author-03.png",
+      name: "Janvi Choudhary",
+      image: "/images/blog/author-01.jpg",
       designation: "Graphic Designer",
     },
-    tags: ["design"],
+    tags: ["info"],
     publishDate: "2025",
   },
 ];
