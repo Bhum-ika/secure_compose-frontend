94271c09ad9af528e27f93e66e9f44c6fdec6147
Bhumika <sharmabhmi@gmail.com>
2023-12-20 15:04:39 +0530

    try demo

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -29,7 +29,7 @@ const Hero = () => {
                   </Link> */}
                   <Link
                     href="https://localhost:3000/authlogin"
-                    className="inline-block rounded-md bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5 hover:translate-x-1"
+                    className="inline-block rounded-md bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-primary/80 dark:text-white dark:hover:bg-white/5 hover:translate-x-1"
                   >
                    Try Demo🔥
                   </Link>
